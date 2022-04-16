ae205fc8af6d794883975146dc8a80ba9ebf85b4
Atefeh-Masoumi <atefeh.masoomy@gmail.com>
2022-04-16 12:09:17 +0430

    logic completed

--- src/component/checkout/Checkout.js
+++ src/component/checkout/Checkout.js
@@ -1,18 +1,50 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../../Providers/AuthProvider";
+import { useCart } from "../../Providers/CartProvider";
+import './checkout.css';
 
 const Checkout = () => {
     const auth = useAuth();
+    const {cart, total} = useCart();
+    if (!cart.length) return <main className="container">
+        <Link to="/">
+        go to Shopping
+        </Link>
+    </main>
     return ( 
-        <div>
-            {auth?
-                <>
-                <p>name: {auth.name}</p>
-            <p>email: {auth.email}</p>
-            <p>tel: {auth.phoneNumber}</p>
-                </>: <p>please login</p>
-            }
 
-        </div>
+        <main className="container" style={{backgroundColor:"#fff"}}>
+            <section className="cartCenter">
+            {auth?
+                    <>
+                    <section className="cartItemList">
+
+                    <h2>checkout detail</h2>
+                    <p>name: {auth.name}</p>
+                    <p>email: {auth.email}</p>
+                    <p>tel: {auth.phoneNumber}</p>
+                    </section>
+                    <section className="cartSummery">
+                        {cart &&
+                        cart.map((c)=>{
+                            return(
+                                <div>
+                                    {c.name} * {c.quantity}: {c.quantity * c.offPrice}
+                                </div>
+                            )
+                        })
+                        }
+                        <hr/>
+                        <div>total: {total}</div>
+                    </section>
+                    </>: <p>please login</p>}
+                
+
+            </section>
+
+            
+
+        </main>
      );
 }
  
